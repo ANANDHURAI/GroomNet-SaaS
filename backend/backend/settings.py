3a8c11d0cf336e2cd4dc4625768311995aaaae77
ANANDHURAI <anand2004d@gmail.com>
2025-08-10 15:36:10 +0530

    whitenoice package added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': 'GroomNetWebsite',
+        'USER': 'postgres',
+        'PASSWORD': 'anand@2004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
