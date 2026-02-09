21bcc5d629a9f06f5d67c0668b80e04d033a4dc7
Anbu Anandhurai <anandhurai2004@gmail.com>
2026-02-09 08:47:22 +0530

    Fix build script and loosen requirements versions

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,11 @@
 
-.\venv\Scripts\activate
+set -o errexit
 
-rm requirements.txt  
+# 1. Install dependencies
+pip install -r requirements.txt
 
+# 2. Collect static files
+python manage.py collectstatic --no-input
 
-pip freeze > requirements.txt
+# 3. Apply database migrations
+python manage.py migrate
